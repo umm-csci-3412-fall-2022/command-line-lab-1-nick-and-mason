57a473d7eac4279405b89f19d3c5da9153e54f56
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-01 16:28:54 -0500

    removed variable

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
-temp_dir=$(mktemp -d -t scratch-XXXi)
-tarchive = $1
-tar -xf tarchive -C ./temp_dir
-
+temp_dir=$(mktemp -d -t scratch-XXX)
+tar -xf $1 -C temp_dir
+cd temp_dir
+echo  grep -l DELETEME! 
 
