8940a5f282c98c14b2e5fc15a08e41d86113d3a8
Nik Bailey <bail0301@morris.umn.edu>
2022-09-06 15:49:06 -0500

    working on loop to delete files

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/bash
 temp_dir=$(mktemp -d -t scratch.XXX)
-echo $temp_dir
 tar -xf $1 -C $temp_dir
 cd $temp_dir
+cd little_dir
 pwd
-echo  grep -l DELETEME!
-echo "testing stuff"
+array=($(ls -d */))
+for i in {0..$(ls | wc -1)}; if[grep -l "DELETE ME!" ]; then
+       	rm ${array[i]}
+else
+	echo "not deleted"
+fi
