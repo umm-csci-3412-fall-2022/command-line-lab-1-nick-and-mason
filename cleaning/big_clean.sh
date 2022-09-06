7d76cce9f0f6ee68333ad2b7ad2c989a41c2ad40
Nik Bailey <bail0301@morris.umn.edu>
2022-09-06 15:54:56 -0500

    got rid of if statement, shaped up loop

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
-temp_dir=$(mktemp -d -t scratch.XXX)
+temp_dir=$(mktemp -d -t escratch.XXX)
 tar -xf $1 -C $temp_dir
 cd $temp_dir
 cd little_dir
 pwd
 array=($(ls -d */))
-for i in {0..$(ls | wc -1)}; if[grep -l "DELETE ME!" ]; then
-       	rm ${array[i]}
-else
-	echo "not deleted"
-fi
+for i in {0..$(ls | wc -1)}
+do 
+rm $(grep -e "DELETE ME!" ${array[i]}
+done
+
