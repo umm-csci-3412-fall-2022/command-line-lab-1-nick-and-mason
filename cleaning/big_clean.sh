b97ccedb0a27ef7f43e484fc77fcf775eff6ecb2
Nik Bailey <bail0301@morris.umn.edu>
2022-09-06 16:22:58 -0500

    set up command with grep and xargs to remove all text files containing delete me

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,10 +3,8 @@ temp_dir=$(mktemp -d -t escratch.XXX)
 tar -xf $1 -C $temp_dir
 cd $temp_dir
 cd little_dir
-pwd
-array=($(ls -d */))
-for i in {0..$(ls | wc -1)}
-do 
-rm $(grep -e "DELETE ME!" ${array[i]}
-done
-
+ls
+echo $(grep -l "DELETE ME!" *)
+grep -l "DELETE ME!" *| xargs rm
+ls
+touch cleaned_little_dir.tgz
