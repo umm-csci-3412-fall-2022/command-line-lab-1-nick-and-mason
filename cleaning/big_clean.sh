45cb75d16c778314b49b494b8753cfba7dd351b0
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-08 16:09:55 -0500

    fixed formatting to appease shellcheck

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 temp_dir=$(mktemp -d -t escratch.XXX)
 here=$(pwd)
-tar -xf $1 -C $temp_dir
-cd $temp_dir
-file_name=$(basename -s .tgz $1)
-cd $file_name
-grep -l "DELETE ME!" *| xargs rm
-cd ..
+tar -xf "$1" -C "$temp_dir"
+cd "$temp_dir" || exit
+file_name=$(basename -s .tgz "$1")
+cd "$file_name" || exit
+grep -l "DELETE ME!" ./*| xargs rm
+cd .. || exit 
 tar -cvzf cleaned_"$1" "$file_name" 
-mv cleaned_"$1" $here
+mv cleaned_"$1" "$here"
